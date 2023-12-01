d6881cf997dbefeb89b1e8f7b6912242c87acc9b
Sinisha <skuhtic@gmail.com>
2023-12-01 22:53:27 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,54 @@
+import kotlin.time.measureTime
+
 fun main() {
     val day = "01"
 
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.fold(0) { a, it ->
+            a + it.first { it.isDigit() }.digitToInt() * 10 + it.last { it.isDigit() }.digitToInt()
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        fun findDigit(part: String): Int? {
+            if (part[0].isDigit()) return part[0].digitToInt()
+            return when {
+                part.startsWith("one") -> 1
+                part.startsWith("two") -> 2
+                part.startsWith("three") -> 3
+                part.startsWith("four") -> 4
+                part.startsWith("five") -> 5
+                part.startsWith("six") -> 6
+                part.startsWith("seven") -> 7
+                part.startsWith("eight") -> 8
+                part.startsWith("nine") -> 9
+                else -> null
+            }
+        }
+
+        fun findDigit(input: String, backwards: Boolean): Int {
+            repeat(input.length) { n ->
+                findDigit(input.drop(if (!backwards) n else input.length - n - 1))?.let {
+                    return it
+                }
+            }
+            error("No first digit in: $input")
+        }
+
+        return input.fold(0) { a, it ->
+            a + findDigit(it, false) * 10 + findDigit(it, true)
+        }
     }
 
-    val testInput = readInput(day, "sample")
-    check(part1(testInput) == 1)
+    readInput(day, "sample").let { checkSample(part1(it), 142) }
+    readInput(day, "sample_p2").let { checkSample(part2(it), 281) }
 
-    val input = readInput(day, "input")
-    part1(input).println()
-    part2(input).println()
+    readInput(day, "input").let {
+        measureTime {
+            part1(it).println()
+            part2(it).println()
+        }.let {
+            println("Done in $it.")
+        }
+    }
 }
