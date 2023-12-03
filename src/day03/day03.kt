31c88d201a622e7936f9a9ac74883cbc9f1d7509
Sinisha <skuhtic@gmail.com>
2023-12-03 23:24:19 +0100

    Day 3

--- src/day03/day03.kt
+++ src/day03/day03.kt
@@ -4,18 +4,28 @@ import utils.*
 
 fun main() {
     val day = "03"
-    fun part1(board: Board): Int {
-        board.logPrint()
-        return 1
-    }
-    fun part2(board: Board): Int = TODO()
+
+    fun part1(board: Board): Int = board.numbers.filter { (p, v) ->
+        p.adjacent(v.toString().length).any {
+            board.symbols.containsKey(it)
+        }
+    }.values.sum()
+
+    fun part2(board: Board): Int = buildMap<Point2D, List<Int>> {
+        val stars = board.symbols.filterValues { it == '*' }.keys
+        board.numbers.forEach { (p, v) ->
+            stars.intersect(p.adjacent(v.toString().length).toSet()).singleOrNull()?.let { star ->
+                set(star, getOrDefault(star, emptyList()) + v)
+            }
+        }
+    }.values.filter { it.size == 2 }.sumOf { it.first() * it.last() }
 
     readInput2(day, "sample").let { Board.parse(it) }.let {
-        checkSample(part1(it), 8)
-        checkSample(part2(it), 2286)
+        checkSample(part1(it), 4361)
+        checkSample(part2(it), 467835)
     }
 
-    readInput(day, "input").let { Board.parse(it) }.let {
+    readInput2(day, "input").let { Board.parse(it) }.let {
         measureSolution {
             part1(it).println()
             part2(it).println()
@@ -25,9 +35,19 @@ fun main() {
     }
 }
 
-data class Pos2D<T>(val pos: Pair<Int, Int>, val value: T)
+typealias Point2D = Pair<Int, Int>
+typealias Map2D<T> = Map<Point2D, T>
 
-typealias Map2D<T> = Map<Pair<Int, Int>, T>
+fun Point2D.adjacent(length: Int = 1) = (first - 1 to first + length).let {
+    buildList {
+        add(it.first to second)
+        add(it.second to second)
+        (it.first..it.second).forEach {
+            add(it to second - 1)
+            add(it to second + 1)
+        }
+    }
+}
 
 data class Board(val numbers: Map2D<Int>, val symbols: Map2D<Char>) {
     companion object {
