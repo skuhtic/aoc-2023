d81438e1910c0194b664ac839de80567582afc62
Sinisha <skuhtic@gmail.com>
2023-12-05 00:10:34 +0100

    Day 4

--- src/day04/Day04.kt
+++ src/day04/Day04.kt
@@ -5,18 +5,28 @@ import utils.*
 fun main() {
     val day = "04"
 
-    fun part1(input: List<Card>): Int = TODO()
+    fun part1(input: List<Card>): Int = input.sumOf {
+        it.wining.intersect(it.numbers).fold(0) { a, i ->
+            if (a == 0) 1 else a * 2
+        }.toInt()
+    }
 
-    fun part2(input: List<Card>): Int = TODO()
+    fun part2(input: List<Card>) = input.associate { it.no to 1 }.toMutableMap().also { counters ->
+        input.forEachIndexed { i, card ->
+            input.drop(i + 1).take(card.wining.intersect(card.numbers).count()).map { it.no }.let { inc ->
+                inc.forEach { counters[it] = counters.getValue(it) + counters.getValue(card.no) }
+            }
+        }
+    }.values.sum()
 
     fun List<String>.mapInput() = map { Card.parse(it) }
 
-    readInput(day, "sample").mapInput().let {
-        checkSample(part1(it), TODO())
-        checkSample(part2(it), TODO())
+    readInput2(day, "sample").mapInput().let {
+        checkSample(part1(it), 13)
+        checkSample(part2(it), 30)
     }
 
-    readInput(day, "input").mapInput().let {
+    readInput2(day, "input").mapInput().let {
         measureSolution {
             part1(it).println()
             part2(it).println()
@@ -24,14 +34,14 @@ fun main() {
     }
 }
 
-data class Card(val no: Int, val wining: List<Int>, val numbers: List<Int>) {
+data class Card(val no: Int, val wining: Set<Int>, val numbers: Set<Int>) {
     companion object {
-        fun parse(line: String): Card = line.split(": ").let {(c, x) ->
+        fun parse(line: String): Card = line.split(": ").let { (c, x) ->
             x.split(" | ", limit = 2).let { nos ->
                 Card(
-                    no = c.substringAfter(' ').toInt(),
-                    wining = nos.first().split( ' ').mapNotNull { it.toIntOrNull() },
-                    numbers = nos.last().split( ' ').mapNotNull { it.toIntOrNull() }
+                    no = c.substringAfterLast(' ').toInt(),
+                    wining = nos.first().split(' ').mapNotNull { it.toIntOrNull() }.toSet(),
+                    numbers = nos.last().split(' ').mapNotNull { it.toIntOrNull() }.toSet()
                 )
             }
         }
